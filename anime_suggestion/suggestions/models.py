cfff6e6ff4c47acdea436036efb24702396b88c7
Eliza Augusta de Carvalho Neta <79981745+ElizaAugusta4@users.noreply.github.com>
2024-04-20 08:59:17 -0300

    Delete anime_suggestion directory

--- anime_suggestion/suggestions/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Anime(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    humor = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
